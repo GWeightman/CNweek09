0b0c2218b04a2bf0b4d7e5f00962cccc55644f43
GWeightman <g.weightman@hotmail.co.uk>
2022-01-04 16:32:14 +0000

    minor changes

--- src/movie/movie_controllers.js
+++ src/movie/movie_controllers.js
@@ -3,7 +3,7 @@ const Movie = require("./movie_model")
 exports.addMovie = async (req, res) => {
     try {
         const mov_entry = await Movie.create(req.body)
-        res.status(201).send({ message: "Success", mov_entry })
+        res.status(201).send({ message: "Successfully added", mov_entry })
     } catch (error) {
         console.log(error)
         res.status(500).send({ message: "check server logs" })
