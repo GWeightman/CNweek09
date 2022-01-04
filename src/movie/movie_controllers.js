813528dc88e085b40647d79a2c7bda80549b1ccc
GWeightman <g.weightman@hotmail.co.uk>
2022-01-04 17:55:19 +0000

    update added

--- src/movie/movie_controllers.js
+++ src/movie/movie_controllers.js
@@ -20,3 +20,23 @@ exports.deleteMovie = async (req, res) => {
     }
 }
 
+exports.editMovie = async (req, res) => {
+    try{
+        const mov_entry = req.body
+        if (mov_entry.title){
+            const movobj = { title: mov_entry.title }
+            const newobj = { $set: {title: mov_entry.new}}
+            await Movie.updateOne(movobj, newobj)
+            res.status(200).send({ message: "Successfully updated", mov_entry })
+        }
+        else if (mov_entry.actor){
+            const movobj = { actor: mov_entry.actor }
+            const newobj = { $set: {actor: mov_entry.new}}
+            await Movie.updateOne(movobj, newobj)
+            res.status(200).send({ message: "Successfully updated", mov_entry })
+        }
+    } catch (error) {
+        console.log (error)
+        res.status(500).send({ message: "check server logs" })
+    }
+}
--- src/movie/movie_routes.js
+++ src/movie/movie_routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express")
 const mov_router = Router()
-const { addMovie, deleteMovie } = require("./movie_controllers")
+const { addMovie, deleteMovie, editMovie } = require("./movie_controllers")
 
 mov_router.post("/movie", addMovie);
 mov_router.delete("/movie", deleteMovie)
+mov_router.put("/movie", editMovie)
 
 module.exports = mov_router
